98cf8697c262e036ec3534b433bfcbfd14c40c8b
Bodia2cat <62301981+Bodia2cat@users.noreply.github.com>
2021-02-24 09:46:04 +0300

    Add windows version for remaster
    Linux version will be soon

---
+++ gamer-master/start.py
@@ -0,0 +1,49 @@
+from tkinter import *
+import os
+root = Tk()
+#=====================functions================
+def check(event):
+	a = e1.get()
+	b = e2.get()
+	if a == "root" or a == "Bodia":
+		if b == "root" or b == "4345":
+			l['text'] = "You are log in"
+			l['fg'] = "green"
+			os.system("python3 logins.py")
+			handle = open("logs", "a")
+			handle.write("\nBodia is log in;")
+			handle.close()
+			os.system("python gam_v3.py BvCk-2045-ztTS")
+		else:
+			l['fg'] = "red"
+			l['text'] = "Uncorrect login or password"
+			handle = open("logs", "a")
+			handle.write("\nPassword from Bodia is incorrect;")
+			handle.close()
+	else:
+		l['fg'] = "red"
+		l['text'] = "Uncorrect login or password;"
+		handle = open("logs", "a")
+		handle.write("\nLogin is incorrect")
+		handle.close()
+
+#=====================objects==================
+l = Label(fg="green", text="All is ok")
+l1 = Label(text="Login: ")
+l2 = Label(text = "Password: ")
+e1 = Entry()
+e2 = Entry()
+b1 = Button(text = "Log in")
+#=====================bind=====================
+
+b1.bind("<Button-1>", check)
+
+#=====================pack=====================
+l.pack()
+l1.pack()
+e1.pack()
+l2.pack()
+e2.pack()
+b1.pack()
+#==============================================
+root.mainloop()

d2b87a23b918b6047e59ad2993b9537328bba29e
Bodia2cat <62301981+Bodia2cat@users.noreply.github.com>
2021-02-25 19:09:30 +0300

    v. 4.5 for linux

---
+++ gamer-master/reg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
+import time
+import os
+import os.path
+import sys
+print("[+] Вы запустили программу регистрации и записи")
+time.sleep(3)
+print("[?] Идет настройка, ожидайте....")
+os.system("bash install.sh")
+print("[+] Настройка системы завершенна, начата обработка данных")
+
+log = ["Bodia", "root"]
+pas = ["4345", "root"]
+class inventory_1:
+	axe = False
+	gun = False
+	life_centre = False
+	key_to_end = False
+class inventory_true:
+	axe = True
+	gun = True
+	life_centre = True
+	key_to_end = False
+time.sleep(2)
+print("[+] Настройка завершенна!")
+print("[?] Начата проверка целостности игры")
+if os.path.exists("gam_v4.py") == True and os.path.exists("install.sh") == True and os.path.exists("LICENSE") == True:
+	print("[+] Проверка окончена! Скрипт завершил роботу")
+else:
+	print("[-] Проверка не пройдена!")
+	sys.exit()

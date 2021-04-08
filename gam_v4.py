ca480591c748217957f007146cdc0653df3f59c8
Bodia2cat <62301981+Bodia2cat@users.noreply.github.com>
2021-04-08 23:04:56 +0300

    Windows Update 2.0
    Version for linux will be later

--- gam_v4.py
+++ gam_v4.py
@@ -1,4 +1,5 @@
 #game v4 //windows update 1.0. 
+import configparser
 from tkinter import *
 from reg import log, pas, inventory_1, inventory_true
 import sys
@@ -17,6 +18,21 @@ print("log " + sys.argv[2])
 print("pas " + sys.argv[3])
 if sys.argv[1]=="BvCk-2045-ztTS" and sys.argv[2]==log[0] or sys.argv[2]==log[1] and sys.argv[3]==pas[0] or sys.argv[3]==pas[1]:
 	print("Login is ok")
+	def open_game(event):
+		config = configparser.ConfigParser()
+		config.read("save.ini")
+		inventory_1.axe = config.get("Game", "axe")
+		inventory_1.gun = config.get("Game", "gun")
+		inventory_1.life_centre = config.get("Game", "med")
+	def save_game(event):
+		config = configparser.ConfigParser()
+		config.add_section("Game")
+		config.set("Game", "axe", str(inventory_1.axe))
+		config.set("Game", "gun", str(inventory_1.gun))
+		config.set("Game", "med", str(inventory_1.life_centre))
+
+		with open("save.ini", "w") as config_file:
+			config.write(config_file)
 	def rando():
 		res1 = ""
 		res2 = ""	
@@ -44,9 +60,10 @@ if sys.argv[1]=="BvCk-2045-ztTS" and sys.argv[2]==log[0] or sys.argv[2]==log[1] 
 			res1 = "нечег нового!!"
 		elif b == 3:
 			res1 = "Рейдеры!!"
-			if sys.argv[2] == "Bodia" and inventory_1.axe == True:
+			if sys.argv[2] == "Bodia" and inventory_1.axe == True or inventory_1.gun == True and sys.argv[2] == "Bodia":
 				res2 = "-Рейдеры"
 				inventory_1.axe == False
+				inventory_1.gun == False
 			if sys.argv[2] == "root":
 				res2 = "-Рейдеры"
 			if sys.argv[2] == "Bodia" and inventory_1.axe == False:
@@ -95,12 +112,18 @@ if sys.argv[1]=="BvCk-2045-ztTS" and sys.argv[2]==log[0] or sys.argv[2]==log[1] 
 	text = Text()
 	b_day = Button(text="След. день")		
 	b_inw = Button(text="Инвентарь")
+	b_save = Button(text="Сохранить игру")
+	b_load = Button(text="Загрузить игру")	
 	#bind
+	b_save.bind("<Button-1>", save_game)
+	b_load.bind("<Button-1>", open_game)
 	b_day.bind("<Button-1>", Next_Day)
 	b_inw.bind("<Button-1>", Inventory_Show)
 	#pack
 	b_day.pack()
 	b_inw.pack()
+	b_save.pack()
+	b_load.pack()
 	text.pack()
 	root.mainloop()	
 else:
